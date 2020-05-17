0b1fdd1ea5bf0c81cf1676406269cc7b355751c5
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-05-17 13:26:42 +0900

    BUGFIX install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "\n\nClone code form git...\n\n"
 mkdir -p ~/glgr_install_temp/
 git clone https://github.com/hsh2001/glgr.git ~/glgr_install_temp/
-cat ~/glgr_install_temp/main.sh >> /usr/local/bin/glgr
+cat ~/glgr_install_temp/main.sh > /usr/local/bin/glgr
 chmod +ux /usr/local/bin/glgr
 cat ~/glgr_install_temp/ascii-art.txt
 rm -rf ~/glgr_install_temp/
