f44c1797619ec9c5d3806025952395f8b51c606b
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-05-17 13:34:59 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function printLog(){
-    git log "$1" --pretty=format:"[%h] %s" --graph
+    git log "$1" --pretty=format:"[%Cblue%h%Creset] %Cgreen\"%s\"%Creset by %Cgreen\"%an\"%Creset" --graph
 }
 
 if [ "$1" = "" ]; then
